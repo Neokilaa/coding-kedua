30252dae949618946d7c3258dc4875fd844ed912
I Gede Putu Sugiatmika <igedeputusugiatmika@Is-MacBook-Pro.local>
2024-07-08 00:16:00 +0700

    first commit

---
+++ knp.py
@@ -0,0 +1,91 @@
+# aritmatika
+tambah = 4 + 2 
+print(tambah)
+kurang = 10 - 3
+print(kurang)
+bagi = 9 / 3
+print(bagi)
+kali = 2 * 4
+print(kali)
+modulo = 25 % 2 
+print(modulo)
+eksponen = 8 ** 4
+print(eksponen)
+
+#plus equal operator
+motor = 2
+motor += 2
+print(motor)
+uang = 100
+uang -= 40
+print(uang)
+
+# if 
+nilai = 85
+if nilai < 80:
+    print("mama marah")
+if nilai > 80:
+    print("mama happy")
+
+baju = "zara"
+if baju == "uniqlo":
+    print("baju uniqlo")
+elif baju == "hnm":
+    print("baju hnm")
+elif baju == "zara":
+    print("baju zara")
+else:
+    print("error")
+
+a = 5
+b = 10
+c = 3
+d = 2
+e = 8
+
+if a + c >= b - d:
+    print("a + c lebih besar atau sama dengan b - d")
+else:
+    print("a + c kurang dari b - d")
+
+if a * d <= b / c:
+    print("a * d kurang dari atau sama dengan b / c")
+else:
+    print("a * d lebih besar dari b / c")
+
+if e - a < d + c:
+    print("e - a kurang dari d + c")
+else:
+    print("e - a lebih besar atau sama dengan d + c")
+
+if e / d > a - c:
+    print("e / d lebih besar dari a - c")
+else:
+    print("e / d kurang dari atau sama dengan a - c")
+
+if "Yes" == "yes":
+    # evaluates to True
+    print("They are equal")
+else: 
+    print("ga sama coy")
+
+c = "2"
+d = 2
+
+if c != d:
+    print("They are equal")
+else:
+    print("They are not equal")
+
+nilai_matkul_a = 80
+nilai_matkul_b = 70
+
+if nilai_matkul_a >= 80 and nilai_matkul_b >= 80:
+    print("Selamat Anda Lulus")
+else:
+    print("Maaf Anda Tidak Lulus")
+
+if nilai_matkul_a >= 80 or nilai_matkul_b >= 80:
+    print("Selamat Anda Lulus")
+else:
+    print("Maaf Anda Tidak Lulus")
